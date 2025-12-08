8770d771a32293bc4d1a39b45967f5433d6aeac6
Ulyana-Nagornaya <ulyana.nagornaya@gmail.com>
2025-12-08 16:06:43 +0300

    fast-api

---
+++ api/app.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from  text_analyzer import Text_Analyzer
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"message": "Speech Analyzer API"}
+
+@app.post("/analyze")
+async def analyze_text(text: str):
+    analyzer = Text_Analyzer(text)
+    analyzer.find_imperatives()
+    analyzer.find_persons()
+    analyzer.detect_questions()
+    
+    return {"result": analyzer.results}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+"""
+curl -X POST "http://127.0.0.1:8000/user" \
+  -H "Content-Type: application/json" \
+  -d '{"name": "Alice", "age": 25}'
+
+`-X POST` - указываем метод
+`-H "Content-Type: application/json"` - говорим серверу, что тело JSON
+`-d '{"name": "Alice", "age": 25}'` - само тело запроса
+"""
