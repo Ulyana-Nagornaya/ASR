7ffcc127984c20ef66582ef76456daa9736254fe
Ulyana-Nagornaya <ulyana.nagornaya@gmail.com>
2025-12-19 21:48:40 +0300

    ruff formater

--- src/text_analyzer/constants.py
+++ src/text_analyzer/constants.py
@@ -1,6 +1,4 @@
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).parent
-INTERROGATIVE_WORDS_PATH = PROJECT_ROOT / 'assets' / 'word lists' / 'interrogative words.txt'
-
-
+INTERROGATIVE_WORDS_PATH = PROJECT_ROOT / "assets" / "word lists" / "interrogative words.txt"
